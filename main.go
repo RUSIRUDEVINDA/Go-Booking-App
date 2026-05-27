ce9bf0d4442117d01524bd07e2155200f38a002d
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-05-27 23:51:36 +0530

    Implement loops in golang

--- main.go
+++ main.go
@@ -1,33 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	conferenceName := "Go Conference"
 	const conferenceTicket = 50
 	var remanningTickets uint = 15
-	var booking []string
+	var bookings []string
 
 	fmt.Printf("Welcome to our %v Booking Application\n", conferenceName)
 	fmt.Println("We have", conferenceTicket, "tickets at the begining, but now only remain", remanningTickets, "tickets")
 	fmt.Println("Get your tickets here to attend")
 
 	for {
-		var userName string
+		var firstName string
+		var lastName string
 		var userTickets uint
 
-		fmt.Printf("Enter your name: ")
-		fmt.Scan(&userName)
+		fmt.Printf("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Printf("Enter your last name: ")
+		fmt.Scan(&lastName)
 
 		fmt.Printf("Enter your ticket count: ")
 		fmt.Scan(&userTickets)
 
 		remanningTickets = remanningTickets - userTickets
-		booking = append(booking, userName)
+		var fullName = firstName + " " + lastName
+		bookings = append(bookings, fullName)
 
-		fmt.Printf("User %v is booked %v tickets.\n", userName, userTickets)
-		fmt.Printf("Remaining Tickets for %v is %v\n", conferenceName, remanningTickets)
-		fmt.Printf("These are our all books %v\n", booking)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Split(booking, " ")
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first names of the bookings are %v\n", firstNames)
+		fmt.Printf("Remaining ticket count is %v\n", remanningTickets)
 	}
 }
